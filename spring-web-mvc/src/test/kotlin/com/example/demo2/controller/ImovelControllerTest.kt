546d1bf9de6c08415f5bb4758edf403b6c28b582
viniciussousazup <vinicius.sousa@zup.com.br>
2020-01-26 16:24:29 -0300

    service test

---
+++ spring-web-mvc/src/test/kotlin/com/example/demo2/controller/ImovelControllerTest.kt
@@ -0,0 +1,19 @@
+package com.example.demo2.controller
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class ImovelControllerTest {
+
+    @Test
+    fun create() {
+    }
+
+    @Test
+    fun listAll() {
+    }
+
+    @Test
+    fun edit() {
+    }
+}
